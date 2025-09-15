a70f08a5852aa70c0eafc167de57ae96d3080a38
Arthur Jamet <arthur.jamet@gmail.com>
2025-09-15 12:11:25 +0200

    Setup Test for CPU instructions

---
+++ test/Internal.hs
@@ -0,0 +1,28 @@
+module Internal (runAndDump) where
+
+import Data.Word
+import Foreign
+import GHC.ForeignPtr (unsafeWithForeignPtr)
+import HNes.Bus
+import HNes.CPU.Interpreter (runProgram)
+import HNes.CPU.State
+import HNes.Memory
+
+-- | Runs a program and returns the state of the CPU at the end of the execution
+runAndDump :: [Word8] -> IO CPUState
+runAndDump program = do
+    fptr <- newMemory
+    writeProgramToMemory program fptr
+    runProgram $ Bus fptr
+
+-- | Writes the given list of bytes at the given pointer
+--
+-- WARNING: The pointer should be allocated to 'memorySize'
+writeProgramToMemory :: [Word8] -> MemoryPointer -> IO ()
+writeProgramToMemory program fptr = unsafeWithForeignPtr fptr (loop program . castPtr)
+  where
+    loop :: [Word8] -> Ptr Word8 -> IO ()
+    loop [] _ = return ()
+    loop (b : bs) ptr = do
+        poke ptr b
+        loop bs (ptr `plusPtr` 1)
