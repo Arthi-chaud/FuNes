658a0a04720f4d1c3e9a38be0f54043963b6b81f
Arthur Jamet <arthur.jamet@gmail.com>
2025-09-17 13:57:45 +0200

    Add STX and STY

--- src/Nes/CPU/Instructions/ST.hs
+++ src/Nes/CPU/Instructions/ST.hs
@@ -1,12 +1,30 @@
-module Nes.CPU.Instructions.ST (sta) where
+module Nes.CPU.Instructions.ST (sta, stx, sty) where
 
 import Nes.CPU.Instructions.Addressing
 import Nes.CPU.Monad
 import Nes.CPU.State
 import Nes.Memory
 
+-- | Store the value of the A register in memory
+--
+-- https://www.nesdev.org/obelisk-6502-guide/reference.html#STA
 sta :: AddressingMode -> CPU r ()
-sta mode = do
+sta = storeRegisterInMemory A
+
+-- | Store the value of the X register in memory
+--
+-- https://www.nesdev.org/obelisk-6502-guide/reference.html#STX
+stx :: AddressingMode -> CPU r ()
+stx = storeRegisterInMemory X
+
+-- | Store the value of the Y register in memory
+--
+-- https://www.nesdev.org/obelisk-6502-guide/reference.html#STY
+sty :: AddressingMode -> CPU r ()
+sty = storeRegisterInMemory Y
+
+storeRegisterInMemory :: Register -> AddressingMode -> CPU r ()
+storeRegisterInMemory reg mode = do
     addr <- getOperandAddr mode
-    regA <- withCPUState registerA
-    withBus $ writeByte regA addr
+    value <- getRegister reg
+    withBus $ writeByte value addr
--- test/CPU/Instructions/STSpec.hs
+++ test/CPU/Instructions/STSpec.hs
@@ -1,17 +1,24 @@
 module CPU.Instructions.STSpec (spec) where
 
-import Foreign
-import GHC.Storable (readWord8OffPtr, writeWord8OffPtr)
+import GHC.Storable (readWord8OffPtr)
 import Internal
 import Nes.CPU.State
 import Test.Hspec
 
 spec :: Spec
 spec = do
-    describe "Register A" $ do
-        it "Using Zero Page addressing" $ do
-            let st = newCPUState{registerA = 0x10}
-                setup ptr = return ()
-            withStateAndMemorySetup [0x85, 0x05, 0x00] st setup $ \_ ptr -> do
-                byte <- readWord8OffPtr ptr 0x05
-                byte `shouldBe` 0x10
+    it "Register A" $ do
+        let st = newCPUState{registerA = 0x10}
+        testWithState st 0x85
+    it "Register X" $ do
+        let st = newCPUState{registerX = 0x10}
+        testWithState st 0x86
+    it "Register Y" $ do
+        let st = newCPUState{registerY = 0x10}
+        testWithState st 0x84
+  where
+    testWithState st opcode = do
+        let setup _ = return ()
+        withStateAndMemorySetup [opcode, 0x05, 0x00] st setup $ \_ ptr -> do
+            byte <- readWord8OffPtr ptr 0x05
+            byte `shouldBe` 0x10
