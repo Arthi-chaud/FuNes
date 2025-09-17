14bc4792349dd7a2c9a40f15a59a824373654f8f
Arthur Jamet <arthur.jamet@gmail.com>
2025-09-17 14:06:54 +0200

    Add LDX and LDY

--- src/Nes/CPU/Instructions/LD.hs
+++ src/Nes/CPU/Instructions/LD.hs
@@ -1,4 +1,4 @@
-module Nes.CPU.Instructions.LD (lda) where
+module Nes.CPU.Instructions.LD (lda, ldx, ldy) where
 
 import Nes.CPU.Instructions.Addressing
 import Nes.CPU.Instructions.After (setZeroAndNegativeFlags)
@@ -6,12 +6,27 @@ import Nes.CPU.Monad
 import Nes.CPU.State
 import Nes.Memory
 
--- | Load Accumulator
+-- | Load Register A
 --
 -- https://www.nesdev.org/obelisk-6502-guide/reference.html#LDA
 lda :: AddressingMode -> CPU r ()
-lda mode = do
+lda = loadRegisterFromMemory A
+
+-- | Load Register X
+--
+-- https://www.nesdev.org/obelisk-6502-guide/reference.html#LDX
+ldx :: AddressingMode -> CPU r ()
+ldx = loadRegisterFromMemory X
+
+-- | Load Register Y
+--
+-- https://www.nesdev.org/obelisk-6502-guide/reference.html#LDY
+ldy :: AddressingMode -> CPU r ()
+ldy = loadRegisterFromMemory Y
+
+loadRegisterFromMemory :: Register -> AddressingMode -> CPU r ()
+loadRegisterFromMemory register mode = do
     argAddr <- getOperandAddr mode
     param <- withBus $ readByte argAddr
-    setRegister A param
+    setRegister register param
     setZeroAndNegativeFlags param
--- test/CPU/Instructions/LDSpec.hs
+++ test/CPU/Instructions/LDSpec.hs
@@ -25,3 +25,14 @@ spec = do
             let setup ptr = writeWord8OffPtr (castPtr ptr) 0x10 0x55
             withMemorySetup [0xa5, 0x10, 0x00] setup $ \cpu _ -> do
                 registerA cpu `shouldBe` 0x55
+    describe "Register X" $ do
+        it "Immediate" $
+            withProgram [0xa2, 0xff, 0x00] $ \cpu -> do
+                registerX cpu `shouldBe` 0xff
+                getStatusFlagPure Negative cpu `shouldBe` True
+    describe "Register Y" $ do
+        it "Immediate" $
+            withProgram [0xa0, 0x05, 0x00] $ \cpu -> do
+                registerY cpu `shouldBe` 0x05
+                getStatusFlagPure Zero cpu `shouldBe` False
+                getStatusFlagPure Negative cpu `shouldBe` False
