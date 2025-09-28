961d84b934d9dcaf926f75254a909db4205c8adc
Arthur Jamet <arthur.jamet@gmail.com>
2025-09-28 10:14:54 +0100

    Add rest of unofficial NOP

--- src/Nes/CPU/Instructions/Noop.hs
+++ src/Nes/CPU/Instructions/Noop.hs
@@ -3,9 +3,12 @@ module Nes.CPU.Instructions.Noop (noop) where
 import Control.Monad
 import Nes.CPU.Instructions.Addressing (AddressingMode (..), getOperandAddr)
 import Nes.CPU.Monad
+import Nes.Memory (MemoryInterface (readByte))
 
 noop :: AddressingMode -> CPU r ()
 noop = \case
     None -> pure ()
     Accumulator -> pure ()
-    mode -> void $ getOperandAddr mode
+    mode -> do
+        addr <- getOperandAddr mode
+        void $ readByte addr ()
--- test/nestest/Spec.hs
+++ test/nestest/Spec.hs
@@ -176,7 +176,7 @@ loadExpectedRawTrace = do
   where
     beforeUnofficialInstr =
         takeWhile
-            (\line -> not $ "C6BF" `BS.isPrefixOf` line)
+            (\line -> not $ "E545" `BS.isPrefixOf` line)
     withoutPPUCycles bs =
         let beforePPU = fst . BS.breakSubstring " PPU:" $ bs
             afterPPU = snd . BS.breakSubstring " CYC:" $ bs
