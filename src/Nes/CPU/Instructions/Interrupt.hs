ee6f668a28f32d9e3bf0332c9c3a3cc35a5a42e7
Arthur Jamet <arthur.jamet@gmail.com>
2025-10-09 17:50:09 +0100

    CPU: Define BRK

---
+++ src/Nes/CPU/Instructions/Interrupt.hs
@@ -0,0 +1,12 @@
+module Nes.CPU.Instructions.Interrupt (brk) where
+
+import Control.Monad
+import Nes.CPU.Monad
+import Nes.CPU.State (CPUState (status), StatusRegisterFlag (InterruptDisable))
+import Nes.FlagRegister (getFlag)
+import Nes.Interrupt
+
+brk :: CPU r ()
+brk = do
+    interruptDisable <- withCPUState $ getFlag InterruptDisable . status
+    unless interruptDisable $ interrupt BRK
