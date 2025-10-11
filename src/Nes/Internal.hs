e0fb0408c1395a2295ae70204b12bf715ac84e17
Arthur Jamet <arthur.jamet@gmail.com>
2025-10-11 16:26:04 +0100

    Rewrite callocForeignPtr

--- src/Nes/Internal.hs
+++ src/Nes/Internal.hs
@@ -1,14 +1,9 @@
 module Nes.Internal (callocForeignPtr) where
 
-import Control.Monad
 import Foreign
-import GHC.ForeignPtr
-import GHC.Storable
 import Nes.Memory (MemoryPointer)
 
 callocForeignPtr :: Int -> IO MemoryPointer
 callocForeignPtr size = do
-    fptr <- mallocForeignPtrBytes size
-    unsafeWithForeignPtr fptr $
-        \ptr -> forM_ [0 .. size] $ \idx -> writeWord8OffPtr ptr idx 0
-    return $ castForeignPtr fptr
+    ptr <- callocBytes size
+    newForeignPtr finalizerFree ptr
