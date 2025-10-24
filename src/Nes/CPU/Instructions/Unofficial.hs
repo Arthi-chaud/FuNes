04a1fb1905d671aec7dd011ef7c6881f867ab450
Arthur Jamet <arthur.jamet@gmail.com>
2025-10-24 11:23:24 +0100

    Add LXA, AXS and LAS

--- src/Nes/CPU/Instructions/Unofficial.hs
+++ src/Nes/CPU/Instructions/Unofficial.hs
@@ -3,14 +3,16 @@
 {-# HLINT ignore "Redundant bracket" #-}
 
 -- | Unofficial instructions that are combinations of official ones
-module Nes.CPU.Instructions.Unofficial (lax, sax, dcp, rra, ahx, shx, shy) where
+module Nes.CPU.Instructions.Unofficial (lax, sax, dcp, rra, ahx, shx, shy, lxa, axs) where
 
 import Control.Monad
 import Data.Bits
+import Nes.CPU.Instructions.Access (lda)
 import Nes.CPU.Instructions.Addressing
 import Nes.CPU.Instructions.After (setZeroAndNegativeFlags)
 import Nes.CPU.Instructions.Arith (addToRegisterA)
 import Nes.CPU.Instructions.Bitwise (ror_)
+import Nes.CPU.Instructions.Transfer
 import Nes.CPU.Monad
 import Nes.CPU.State
 import Nes.FlagRegister
@@ -54,8 +56,19 @@ rra mode = do
     value <- ror_ mode
     addToRegisterA value
 
+-- | Source: https://forums.nesdev.org/viewtopic.php?t=8107
 shx :: AddressingMode -> CPU r ()
-shx = sh X
+shx mode = do
+    addr <- getOperandAddr mode
+    x <- withCPUState $ getRegister X
+    y <- withCPUState $ getRegister Y
+    let value = (x .&. unsafeAddrToByte (((shiftR addr 8) + 1)))
+        tmp = (addr - (byteToAddr y)) .&. 0xff
+    if ((byteToAddr y) + tmp <= 0xff)
+        then writeByte value addr ()
+        else do
+            res <- readByte addr ()
+            writeByte res addr ()
 
 shy :: AddressingMode -> CPU r ()
 shy = sh Y
@@ -75,3 +88,20 @@ sh reg mode = do
     addr <- getOperandAddr mode
     let byte' = mask .&. ((unsafeAddrToByte (shiftR addr 8)))
     writeByte byte' addr ()
+
+lxa :: AddressingMode -> CPU r ()
+lxa = lda >=> const tax
+
+axs :: AddressingMode -> CPU r ()
+axs mode = do
+    addr <- getOperandAddr mode
+    byte <- readByte addr ()
+    x <- withCPUState $ getRegister X
+    a <- withCPUState $ getRegister A
+    let xAndA = x .&. a
+    let res = xAndA - byte
+    modifyCPUState $
+        modifyStatusRegister
+            (setFlag' Carry (byte <= xAndA))
+            . setRegister X res
+    setZeroAndNegativeFlags res
