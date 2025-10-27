66ae1301cb949d6e684b17c37611ee0fad328466
Arthur Jamet <arthur.jamet@gmail.com>
2025-10-27 08:47:41 +0000

    APU: Pulse: Doc fields + export

--- src/Nes/APU/State/Pulse.hs
+++ src/Nes/APU/State/Pulse.hs
@@ -1,6 +1,25 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module Nes.APU.State.Pulse where
+module Nes.APU.State.Pulse (
+    -- * Pulse definition
+    Pulse (..),
+    PulseByte (..),
+    setPulseByte,
+    withPulseByte,
+
+    -- * Fields
+    PulseField,
+    duty,
+    loops,
+    volumeIsConst,
+    volume,
+    sweepEnabled,
+    sweepPeriod,
+    sweepNegate,
+    sweepShift,
+    timer,
+    lengthCounterLoad,
+) where
 
 import Data.Bits (Bits (..))
 import Data.Word (Word16)
@@ -9,11 +28,13 @@ import Nes.Memory (Addr (unAddr), Byte (..), byteToAddr)
 
 data Pulse = MkPulse
     { byte1 :: Byte
+    -- ^ The first register byte of the pulse. E.g. if this is the first pulse register, Bus should be able to access it at 0x4000
     , byte2 :: Byte
     , byte3 :: Byte
     , byte4 :: Byte
     }
 
+-- | Enum to get a single byte from a 'Pulse'
 data PulseByte
     = Byte1
     | Byte2
@@ -21,6 +42,7 @@ data PulseByte
     | Byte4
     deriving (Eq, Show)
 
+-- | Update a given byte from a 'Pulse'
 setPulseByte :: PulseByte -> (Byte -> Byte) -> Pulse -> Pulse
 setPulseByte n f pulse = case n of
     Byte1 -> pulse{byte1 = f $ byte1 pulse}
@@ -28,6 +50,7 @@ setPulseByte n f pulse = case n of
     Byte3 -> pulse{byte3 = f $ byte3 pulse}
     Byte4 -> pulse{byte4 = f $ byte4 pulse}
 
+-- | Get a byte (and optionally do something with it) from a 'Pulse'
 withPulseByte :: PulseByte -> (Byte -> a) -> Pulse -> a
 withPulseByte n f pulse = case n of
     Byte1 -> f $ byte1 pulse
@@ -35,71 +58,94 @@ withPulseByte n f pulse = case n of
     Byte3 -> f $ byte3 pulse
     Byte4 -> f $ byte4 pulse
 
-duty :: BitField (Bit, Bit) Pulse
+type PulseField a = BitField a Pulse
+
+-- | Duty cycles. Is on bits 6 and 7 of byte 1 of Pulse
+--
+-- Note: Only the first 2 bits of the input Byte are considered
+--
+-- Source: https://www.nesdev.org/wiki/APU#Pulse_($4000–$4007)
+duty :: PulseField Byte
 duty = MkBitField{..}
   where
     get =
         withPulseByte
             Byte1
-            (\b -> (getBit' 7 b, getBit' 6 b))
-    set (high, low) =
+            (`shiftR` 6)
+    set byte =
         setPulseByte
             Byte1
             (setBit' high 7 . setBit' low 6)
+      where
+        high = fromBool $ testBit byte 1
+        low = fromBool $ testBit byte 0
 
-loops :: BitField Bool Pulse
+-- | Envelope loop. Is on bit 5 of byte 1 of Pulse
+--
+-- Source: https://www.nesdev.org/wiki/APU#Pulse_($4000–$4007)
+loops :: PulseField Bool
 loops = singleBitField Byte1 5
 
-volumeIsConst :: BitField Bool Pulse
+-- | Volume is const . Is on bit 4 of byte 1 of Pulse
+--
+-- Source: https://www.nesdev.org/wiki/APU#Pulse_($4000–$4007)
+volumeIsConst :: PulseField Bool
 volumeIsConst = singleBitField Byte1 4
 
--- | Volume envelope.
+-- | Volume envelope. On first 4 bits of byte 1 of Pulse
 --
--- Only the 4 first bits are taken
-volume :: BitField Byte Pulse
+-- Note: Only the 4 first bits of input are taken
+--
+-- Source: https://www.nesdev.org/wiki/APU#Pulse_($4000–$4007)
+volume :: PulseField Byte
 volume = MkBitField{..}
   where
     get = withPulseByte Byte1 first4bits
     set vol = setPulseByte Byte1 $ \b -> (b .&. 0b00001111) .|. first4bits vol
     first4bits b = b .&. 0b1111
 
-sweepEnabled :: BitField Bool Pulse
+-- | Sweep is enabled. On bit 7 of byte 2 of Pulse
+--
+-- Source: https://www.nesdev.org/wiki/APU_Sweep
+sweepEnabled :: PulseField Bool
 sweepEnabled = singleBitField Byte2 7
 
-sweepNegate :: BitField Bool Pulse
+-- | Sweep is negated. On bit 3 of byte 2 of Pulse
+--
+-- Source: https://www.nesdev.org/wiki/APU_Sweep
+sweepNegate :: PulseField Bool
 sweepNegate = singleBitField Byte2 3
 
--- | Sweep Period
+-- | Sweep Period. On bits 4-6 of byte 2 of Pulse
 --
 -- The divider's period is P + 1 half-frames
 --
--- Only the first 3 bits are taken
-sweepPeriod :: BitField Byte Pulse
+-- Note: Only the first 3 bits are taken
+--
+-- Source: https://www.nesdev.org/wiki/APU_Sweep
+sweepPeriod :: PulseField Byte
 sweepPeriod = MkBitField{..}
   where
     get = withPulseByte Byte2 $ \b -> (b `shiftR` 4) .&. 0b111
     set vol = setPulseByte Byte1 $
         \b -> (b .&. 0b10001111) .|. ((vol .&. 0b111) `shiftL` 4)
 
--- | Sweep Shift
+-- | Sweep Shift. On bits 0-2 of byte 2 of Pulse
 --
--- Only the first 3 bits are taken
-sweepShift :: BitField Byte Pulse
+-- Note: Only the first 3 bits are taken
+--
+-- Source: https://www.nesdev.org/wiki/APU_Sweep
+sweepShift :: PulseField Byte
 sweepShift = MkBitField{..}
   where
     get = withPulseByte Byte2 $ \b -> b .&. 0b111
     set shiftCount = setPulseByte Byte1 $
         \b -> (b .&. 0b11111000) .|. (shiftCount .&. 0b111)
 
-singleBitField :: PulseByte -> Int -> BitField Bool Pulse
-singleBitField byte off
-    | off < 0 || off >= 8 = error "Invalid bit offset in byte"
-    | otherwise = MkBitField{..}
-  where
-    get = withPulseByte byte $ toBool . getBit' off
-    set b = setPulseByte byte $ setBit' (fromBool b) off
-
-timer :: BitField Word16 Pulse
+-- | Timer. On byte 3 of Pulse (low) and bits 0-2 of byte 4 (high)
+--
+-- Source: https://www.nesdev.org/wiki/APU#Pulse_($4000–$4007)
+timer :: PulseField Word16
 timer = MkBitField{..}
   where
     get pulse =
@@ -116,8 +162,20 @@ timer = MkBitField{..}
             setPulseByte Byte4 (\b -> (b .&. 0b11111000) .&. high)
                 . setPulseByte Byte3 (const low)
 
-lengthCounterLoad :: BitField Byte Pulse
+-- | Length counter load. On bits 3-7 of byte 4 of Pulse
+--
+-- Source: https://www.nesdev.org/wiki/APU#Pulse_($4000–$4007)
+lengthCounterLoad :: PulseField Byte
 lengthCounterLoad = MkBitField{..}
   where
     get = withPulseByte Byte4 $ \b -> (b .&. 0b11111000) `shiftR` 3
-    set l = setPulseByte Byte4 $ \b -> (b .&. 0b111) .|. (l `shiftL` 3)
+    set l = setPulseByte Byte4 $ \b -> (l `shiftL` 3) .|. (b .&. 0b111)
+
+-- | Util for single-bit fields
+singleBitField :: PulseByte -> Int -> PulseField Bool
+singleBitField byte off
+    | off < 0 || off >= 8 = error "Invalid bit offset in byte"
+    | otherwise = MkBitField{..}
+  where
+    get = withPulseByte byte $ toBool . getBit' off
+    set b = setPulseByte byte $ setBit' (fromBool b) off
