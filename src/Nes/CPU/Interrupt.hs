3f864575e789fcd26dea83ce0e4f4b2e7d1f36b6
Arthur Jamet <arthur.jamet@gmail.com>
2025-11-18 15:02:28 +0000

    Restore DMC DMA

--- src/Nes/CPU/Interrupt.hs
+++ src/Nes/CPU/Interrupt.hs
@@ -1,5 +1,11 @@
 module Nes.CPU.Interrupt (handleInterrupt) where
 
+import Control.Monad
+import Nes.APU.Monad (modifyAPUStateWithInterrupt)
+import Nes.APU.State (APUState (dmc), modifyDMC')
+import Nes.APU.State.DMC (DMC (sampleBufferAddr), loadSampleBuffer)
+import Nes.Bus (Bus (apuState))
+import qualified Nes.Bus.Monad as BusM
 import Nes.CPU.Monad
 import Nes.CPU.State
 import Nes.FlagRegister
@@ -22,3 +28,8 @@ handleInterruptSignal signal = do
     pushStatusRegister (pushesBFlag signal)
     modifyCPUState $ modifyStatusRegister $ setFlag InterruptDisable
     setPC =<< readAddr (getVectorAddr signal) ()
+    -- Ugly, shouldn't be here
+    when (signal == IRQ DMC) $ withBus $ do
+        sampleByteAddr <- BusM.withBus $ sampleBufferAddr . dmc . apuState
+        sample <- Nes.Memory.readByte sampleByteAddr ()
+        BusM.withAPU $ modifyAPUStateWithInterrupt $ modifyDMC' $ loadSampleBuffer sample
