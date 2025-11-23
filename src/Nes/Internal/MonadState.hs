4133220ccde72686d1fdc143945ddf05a3bbc5c1
Arthur Jamet <arthur.jamet@gmail.com>
2025-11-23 10:46:26 +0000

    Define MonadState + Instance for CPU monad

---
+++ src/Nes/Internal/MonadState.hs
@@ -0,0 +1,21 @@
+module Nes.Internal.MonadState (MonadState (..), modify, modify', gets) where
+
+-- | Very similar to mtl's StateMonad, however we don't use functional dependencies to resolve the type of the state. Monads that have multiple instances for this class will have to use explicit type application
+class (Monad m) => MonadState s m where
+    get :: m s
+    set :: s -> m ()
+
+{-# INLINE modify #-}
+modify :: (MonadState s m) => (s -> s) -> m ()
+modify f = get >>= set . f
+
+{-# INLINE modify' #-}
+modify' :: (MonadState s m) => (s -> (a, s)) -> m a
+modify' f = do
+    (a, s) <- gets f
+    set s
+    return a
+
+{-# INLINE gets #-}
+gets :: (MonadState s m) => (s -> a) -> m a
+gets f = fmap f get
